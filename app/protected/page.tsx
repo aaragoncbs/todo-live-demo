eb7c5f089fcdfc5e8f1d0872d35c591bdea361e6
root <root@srv989450.hstgr.cloud>
2025-09-06 18:58:21 +0000

    page.tsx Ok

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -1,4 +1,16 @@
+"use client";
+import { useState } from "react";
+import AddToDo from "@/components/AddToDo";
+import ToDoList from "@/components/ToDoList";
+
 export default function Home() {
-return <div>Hello World</div>;
+  const [refreshTodos, setRefreshTodos] = useState<(() => Promise<void>) | nul
+  
+  return (
+    <div>
+      <AddToDo onAdd={() => refreshTodos && refreshTodos()} />
+      <ToDoList onReady={(fetchTodos) => setRefreshTodos (() => fetchTodos)} />
+    </div>
+  );
 }
 
